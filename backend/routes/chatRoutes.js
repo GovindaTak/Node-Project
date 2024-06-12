c970bb53f237b374364217472318ec3b905657f4
Govinda-Tak <govinda.tak@atmecs.com>
2024-06-12 11:48:14 +0530

    Update chatRoutes.js

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const { uploadMultiple,handleQuery } = require('../controllers/chatController');
-
+const {authenticateUser,authorizeDepartment}=require('../middlewares/authMiddleware')
 
 const router = express.Router();
 
@@ -18,6 +18,6 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 router.post('/upload-multiple', upload.array('pdfs', 3), uploadMultiple);
-router.post('/handleQuery', handleQuery);
+router.post('/handleQuery',authenticateUser, handleQuery);
 
 module.exports = router;
